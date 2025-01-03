71dc0844c4cfa6093f312236aefe4e4de1663ee5
yahya <yahyaelhammali47@gmail.com>
2025-01-03 22:26:37 +0100

    puch cicd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+//import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+//export default defineConfig({
+  //plugins: [react()],
+//})
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', 
+  build: {
+    outDir: 'dist',
+  },
+});
