6b14ea9a12fe2d2b8253da0403bd9855f10d11cc
yahya <yahyaelhammali47@gmail.com>
2025-01-03 22:53:21 +0100

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,8 @@
-//import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-//export default defineConfig({
-  //plugins: [react()],
-//})
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'; // Gardez uniquement cette ligne
-
-// Configuration de Vite
 export default defineConfig({
   plugins: [react()],
 });
+
